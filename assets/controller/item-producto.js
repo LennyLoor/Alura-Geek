4c549b4f9d29df2665989ce9b33978df4e01ee42
Melizloor <lenny_loor@outlook.com>
2023-09-17 16:36:59 -0500

    enlace-imagen

--- assets/controller/item-producto.js
+++ assets/controller/item-producto.js
@@ -2,7 +2,7 @@ export const itemProducto = (categoria, name, imageUrl, price, id) => {
     const item_ = document.createElement('li');
     item_.setAttribute('class', 'product__list__item');
     const content_ = `<div class="list__item--imagen">  
-                            <img alt="imagen-${categoria}" src="${imageUrl}">
+                            <img alt="imagen-${categoria}" src="../${imageUrl}">
                         </div>
                         <span class="list__item--nombre">${name}</span>
                         <span class="list__item--precio"> ${price} </span>
